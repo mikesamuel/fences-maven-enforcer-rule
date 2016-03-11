9ca703d069dfc42118a1507165cf0a0297f44220
Mike Samuel <mikesamuel@gmail.com>
2016-03-11 08:43:52 -0500

    use version instead of baseversion

--- src/main/java/com/google/security/fences/util/Utils.java
+++ src/main/java/com/google/security/fences/util/Utils.java
@@ -10,8 +10,10 @@ public final class Utils {
   public static String artToString(Artifact art) {
     // TODO: Replace once we update to a version of ArtifactUtils that has
     // key(Artifact)?
+    // Is this redundant with Artifact.getId()?  The format of that is not
+    // documented.
     return art.getGroupId() + ":" + art.getArtifactId()
-        + ":" + art.getBaseVersion();
+        + ":" + art.getVersion();
   }
 
   private Utils() {
