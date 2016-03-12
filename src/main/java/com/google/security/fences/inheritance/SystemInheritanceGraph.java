6c1abe717eb7550d8f3891610bf2694ebe1041b6
Mike Samuel <mikesamuel@gmail.com>
2016-03-12 09:45:58 -0500

    IDE warning cleanup

--- src/main/java/com/google/security/fences/inheritance/SystemInheritanceGraph.java
+++ src/main/java/com/google/security/fences/inheritance/SystemInheritanceGraph.java
@@ -14,7 +14,6 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassVisitor;
