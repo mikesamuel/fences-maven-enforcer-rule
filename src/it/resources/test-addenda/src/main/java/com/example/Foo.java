d84aee806b4d58b3082fc019f3111807043c9a36
Mike Samuel <mikesamuel@gmail.com>
2016-04-02 00:46:54 -0400

    added Findbugs annotations to integration tests because Travis-CI seems to run findbugs on those projects as well.

--- src/it/resources/test-addenda/src/main/java/com/example/Foo.java
+++ src/it/resources/test-addenda/src/main/java/com/example/Foo.java
@@ -1,8 +1,11 @@
 package com.example;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
 public class Foo {
   public final String s;
 
+  @SuppressFBWarnings("DM_EXIT")
   public Foo(String s) {
     if (s == null) { System.exit(-1); }
     this.s = s;
--- src/it/resources/test-imports/unsafe-api/src/main/java/com/example/api/Unsafe.java
+++ src/it/resources/test-imports/unsafe-api/src/main/java/com/example/api/Unsafe.java
@@ -1,6 +1,9 @@
 package com.example.api;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
 public class Unsafe {
+  @SuppressFBWarnings("DM_EXIT")
   public Unsafe pushRedButton() {
     for (int i = 10; --i >= 0;) {
       System.err.println("T - " + i);
