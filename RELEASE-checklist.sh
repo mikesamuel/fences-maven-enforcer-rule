d23153f0f5dbf84643226951e45f2e6fb5ceb027
Mike Samuel <mikesamuel@gmail.com>
2016-04-25 10:34:17 -0400

    autopick new version numbers

--- RELEASE-checklist.sh
+++ RELEASE-checklist.sh
@@ -23,10 +23,6 @@ echo
 echo Check https://search.maven.org/#search%7Cga%7C1%7Cowasp-java-html-sanitizer
 echo and make sure that the current POM release number is max.
 
-# Pick a release version
-export NEW_VERSION=1.4-beta
-export NEW_DEV_VERSION=1.5-beta-SNAPSHOT
-
 cd ~/work
 export RELEASE_CLONE="$PWD/fences-maven-enforcer-rule-release"
 rm -rf "$RELEASE_CLONE"
@@ -34,6 +30,26 @@ cd "$(dirname "$RELEASE_CLONE")"
 git clone git@github.com:mikesamuel/fences-maven-enforcer-rule.git \
     "$(basename "$RELEASE_CLONE")"
 cd "$RELEASE_CLONE"
+
+# Pick a release version
+export OLD_VERSION="$(perl -e '
+  use strict;
+  use XML::LibXML;
+  my $dom = XML::LibXML->new->parse_file($ARGV[0]);
+  my $xml = XML::LibXML::XPathContext->new($dom);
+  $xml->registerNs(qq[mvn], qq[http://maven.apache.org/POM/4.0.0]);
+  for my $node ($xml->findnodes(qq[/mvn:project/mvn:version/text()])) {
+    print $node->toString;
+  }' \
+  "$RELEASE_CLONE/pom.xml")"
+export NEW_VERSION="$(echo -n "$OLD_VERSION" | perl -pe 's/-SNAPSHOT$//')"
+export NEW_DEV_VERSION="$(perl -e '
+  $_ = $ARGV[0]; s/(\d+)(\D*)$/($1 + 1) . $2/e; print' \
+  "$OLD_VERSION")"
+
+echo "OLD_VERSION=$OLD_VERSION"
+echo "NEW_VERSION=$NEW_VERSION"
+echo "NEW_DEV_VERSION=$NEW_DEV_VERSION"
 
 # Update the version
 # mvn release:update-versions puts -SNAPSHOT on the end no matter what
